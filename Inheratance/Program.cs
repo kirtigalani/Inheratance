4418cc6535a5e3bea401648ed780883c75637911
kirtigalani <kittugalani@gmail.com>
2024-12-04 18:32:16 +0530

    Add project files.

---
+++ Inheratance/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Inheratance
+{
+    public class Parents
+    {
+        public Parents()
+        {
+            Console.WriteLine("ParentClass constructor called");
+
+        }
+        public Parents(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+    public class child : Parents
+    {
+        public child() : base("Derived class constrolling parent class")
+        {
+            Console.WriteLine("child class constructor called");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            child C = new child();  
+
+        }
+    }
+}
